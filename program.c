224668e9769d833efd26e11bfa7b57eeca60b811
nb23506 <167288305+nb23506@users.noreply.github.com>
2024-09-30 15:12:14 +0900

    Update program.c

--- program.c
+++ program.c
@@ -1,4 +1,23 @@
-// program.c
-int add(int a, int c) {
-    return a + c;
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s <integer x> <integer y>\n", argv[0]);
+        return 1;
+    }
+
+    int x = atoi(argv[1]); // Convert the first argument to an integer
+    int y = atoi(argv[2]); // Convert the second argument to an integer
+
+    // Use if statement to determine the larger number
+    if (x > y) {
+        printf("The larger number is: %d\n", x);
+    } else if (y > x) {
+        printf("The larger number is: %d\n", y);
+    } else {
+        printf("Both numbers are equal: %d\n", x);
+    }
+
+    return 0;
 }
