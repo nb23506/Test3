13cc23a4eceafd037b88808341810d65a22342dc
nb23506 <167288305+nb23506@users.noreply.github.com>
2024-09-30 15:21:05 +0900

    Update program.c

--- program.c
+++ program.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int larger_number(int x, int y) {
+    if (x > y) {
+        return x;
+    } else if (y > x) {
+        return y;
+    } else {
+        return -1; // Menandakan bahwa kedua angka sama
+    }
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        printf("Usagee: %s <integer x> <integer y>\n", argv[0]);
+        printf("Usage: %s <integer x> <integer y>\n", argv[0]);
         return 1;
     }
 
-    int x = atoi(argv[1]); // Convert the first argument to an integer
-    int y = atoi(argv[2]); // Convert the second argument to an integer
-
-    // Use if statement to determine the larger number
-    if (x > y) {
-        printf("The larger number is: %d\n", x);
-    } else if (y > x) {
-        printf("The larger number is: %d\n", y);
-    } else {
+    int x = atoi(argv[1]);
+    int y = atoi(argv[2]);
+    
+    int result = larger_number(x, y);
+    
+    if (result == -1) {
         printf("Both numbers are equal: %d\n", x);
+    } else {
+        printf("The larger number is: %d\n", result);
     }
 
     return 0;
