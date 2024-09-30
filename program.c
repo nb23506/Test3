299147f3b9b91f689cc1552e4977cf09987dee66
nb23506 <167288305+nb23506@users.noreply.github.com>
2024-09-30 15:18:48 +0900

    Update program.c

--- program.c
+++ program.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        printf("Usage: %s <integer x> <integer y>\n", argv[0]);
+        printf("Usagee: %s <integer x> <integer y>\n", argv[0]);
         return 1;
     }
 
